8045c8798d21b5add1799c6f130fd241214d467c
Tristan Soler <tristan.soler@santanderam.com>
2024-10-29 12:12:52 +0100

    modified the script that removed the duplicates to use the library pathlib and the class Path to define input and output paths

--- remove_duplicates_with_ovr.py
+++ remove_duplicates_with_ovr.py
@@ -1,7 +1,8 @@
 import logging
 import time
+from pathlib import Path
 
-import pandas
+import pandas as pd
 
 
 # Set up logging info
@@ -17,41 +18,44 @@ start_time = time.time()
 logging.info("Script started")
 
 # Define Date
-DATE = input("Insert data in the formar YYYYMM, please: ")
+DATE = input("Insert date in the format YYYYMM, please: ")
 
-# define BASE_DIRECTORY   C:\Users\n740789\Documents\Projects_local\DataSets\DATAFEED\datafeeds_with_ow
-BASE_DIRECTORY = (
+# Define BASE_DIRECTORY
+BASE_DIRECTORY = Path(
     r"C:\Users\n740789\Documents\Projects_local\DataSets\DATAFEED\datafeeds_with_ovr"
 )
-# define INPUT_PATH = BASE_DIRECTORY \ r'20240701_datafeed_with_ow.csv'
-INPUT_PATH = BASE_DIRECTORY + rf"\{DATE}01_datafeed_with_ovr.csv"
-# define OUTPU_PATH
-OUTPUT_PATH = BASE_DIRECTORY + rf"\{DATE}_df_issuer_level_with_OVR.xlsx"
-# define OUTPUT_PATH for CSV file
-OUTPUT_PATH_CSV = BASE_DIRECTORY + rf"\{DATE}_df_issuer_level_with_ovr.csv"
+
+# Define INPUT_PATH
+INPUT_PATH = BASE_DIRECTORY / f"{DATE}01_datafeed_with_ovr.csv"
+
+# Define OUTPUT_PATH for Excel file
+OUTPUT_PATH = BASE_DIRECTORY / f"{DATE}_df_issuer_level_with_OVR.xlsx"
+
+# Define OUTPUT_PATH for CSV file
+OUTPUT_PATH_CSV = BASE_DIRECTORY / f"{DATE}_df_issuer_level_with_ovr.csv"
 
 logging.info("Loading raw dataset")
-# read csv INPUT_PATH
-df = pandas.read_csv(INPUT_PATH, low_memory=False)
+# Read csv INPUT_PATH
+df = pd.read_csv(INPUT_PATH, low_memory=False)
 
 # Lower column names
 df.columns = df.columns.str.lower()
 
 logging.info("Removing duplicates by permId")
-# remove duplicate by subset "issuer_name"
+# Remove duplicate by subset "permid"
 df_2 = df.drop_duplicates(subset=["permid"])
 
-
-# save to OUTPUT_PATH as csv file
+# Save to OUTPUT_PATH as csv file
 logging.info("Saving dataset at issuer level on a csv file")
 df_2.to_csv(OUTPUT_PATH_CSV, index=False)
 
-# save to OUTPUT_PATH as excel file
+# Save to OUTPUT_PATH as excel file
+# Uncomment the following lines if you want to save as Excel file
 # logging.info("Saving dataset at issuer level on an Excel file")
 # df_2.to_excel(OUTPUT_PATH, index=False)
 
 end_time = time.time()
 logging.info(f"Script completed in {end_time - start_time:.2f} seconds")
 logging.info(f"Files saved in {BASE_DIRECTORY}")
-# display rows before and after
+# Display rows before and after
 logging.info(f"\nRows before: {df.shape[0]} \nRows after: {df_2.shape[0]}")
--- remove_duplicates_wo_ovr.py
+++ remove_duplicates_wo_ovr.py
@@ -1,5 +1,6 @@
 import logging
 import time
+from pathlib import Path
 
 import pandas
 
@@ -16,21 +17,25 @@ setup_logging()
 start_time = time.time()
 logging.info("Script started")
 
-
 # Define Date
 DATE = input("Insert data in the formar YYYYMM, please: ")
 
-# define BASE_DIRECTORY   C:\Users\n740789\Documents\Projects_local\DataSets\DATAFEED
-BASE_DIRECTORY = r"C:\Users\n740789\Documents\Projects_local\DataSets\DATAFEED"
-# define INPUT_PATH = BASE_DIRECTORY \ r'20240701_datafeed_with_ow.csv'
+# define BASE_DIRECTORY
+BASE_DIRECTORY = Path(r"C:\Users\n740789\Documents\Projects_local\DataSets\DATAFEED")
+
+# Define INPUT_PATH
 INPUT_PATH = (
     BASE_DIRECTORY
-    + rf"\raw_dataset\{DATE}01_Production\{DATE}01_Equities_feed_new_strategies_filtered_old_names_iso_permId.csv"
+    / "raw_dataset"
+    / f"{DATE}01_Production"
+    / f"{DATE}01_Equities_feed_new_strategies_filtered_old_names_iso_permId.csv"
 )
-# define OUTPU_PATH DIRECTORY:
+
+# Define OUTPUT_PATH
 OUTPUT_PATH = (
     BASE_DIRECTORY
-    + rf"\ficheros_tratados\{DATE}01_Equities_feed_IssuerLevel_sinOVR.xlsx"
+    / "ficheros_tratados"
+    / f"{DATE}01_Equities_feed_IssuerLevel_sinOVR.xlsx"
 )  # naming 20240901_Equities_feed_IssuerLevel_sinOVR
 
 logging.info("Loading raw dataset")
@@ -44,12 +49,13 @@ logging.info("Removing duplicates by permId")
 # remove duplicate by subset "issuer_name"
 df_2 = df.drop_duplicates(subset=["permid"])
 
-# save to OUTPUT_PATH as csv file
+# Save to OUTPUT_PATH as csv file
 logging.info("Saving dataset at issuer level on a csv file")
-df_2.to_csv(OUTPUT_PATH.replace(".xlsx", ".csv"), index=False)
+csv_output_path = OUTPUT_PATH.with_suffix(".csv")
+df_2.to_csv(csv_output_path, index=False)
 
 logging.info("Saving dataset at issuer level on an Excel file")
-# save to OUTPUT_PATH as excel file
+# Save to OUTPUT_PATH as excel file
 df_2.to_excel(OUTPUT_PATH, index=False)
 
 end_time = time.time()
