8d9b46c824c5d549792efd693e7fa3be86f7e6bf
Tristan Soler <tristan.soler@santanderam.com>
2024-11-11 11:07:33 +0100

    added new feature to the run datapipelin script where now the scrpipt prints at the end how long it takes to run

--- shell_scripts/run_datapipeline.sh
+++ shell_scripts/run_datapipeline.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Record the start time
+start_time=$(date +%s)
+
 # Check if date parameter is provided
 if [ $# -eq 0 ]; then
     echo "Please provide a date parameter (format: yyyymm)"
     echo "Example: shell_scripts/run_datapipeline.sh 202411"
     exit 1
 fi
-
+# Assign the first parameter to the DATE variable
 DATE=$1
 
 # Array of scripts to run
@@ -16,6 +19,7 @@ SCRIPTS=(
     "/c/Users/n740789/Documents/clarity_data_quality_controls/02_apply_ow.py"
     "/c/Users/n740789/Documents/clarity_data_quality_controls/03_remove_duplicates_with_ovr.py"
     "/c/Users/n740789/Documents/clarity_data_quality_controls/04_noncomplience.py"
+    "/c/Users/n740789/Documents/clarity_data_quality_controls/05_impact_analysis.py"
 )
 
 # Execute Python scripts in sequence, passing the date parameter
@@ -47,3 +51,10 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Data pipeline completed successfully"
+
+# Calculate and display the total execution time
+end_time=$(date +%s)
+total_time=$((end_time - start_time))
+minutes=$((total_time / 60))
+seconds=$((total_time % 60))
+echo "Time: $minutes min, $seconds seconds."
