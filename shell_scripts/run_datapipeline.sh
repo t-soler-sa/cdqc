5535ff1c2a0843c53fc8ffdf57135a984773a5fa
Tristan Soler <tristan.soler@santanderam.com>
2025-04-11 18:04:48 +0200

    fixed minor bug form the run datapipeline script where I was missing an "s" from the name of a script

--- shell_scripts/run_datapipeline.sh
+++ shell_scripts/run_datapipeline.sh
@@ -24,8 +24,7 @@ BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
 
 # Array of scripts to run
 SCRIPTS=(
-    "_00_preovr_analysis.py"
-    "_01_generate_ovr_list.py"
+    "_01_generate_ovr_lists.py"
     "_02_apply_ovr.py"
     "_03_noncompliance_analysis.py"
     "_04_impact_analysis.py"
