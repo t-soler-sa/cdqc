f636489ba0ef86caa6cb8760015c799343907b48
Tristan Soler <tristan.soler@santanderam.com>
2025-04-16 17:47:01 +0200

    masssive changes to the script that runs the pre-ovr process. But now it works!!

--- shell_scripts/run_pre_ovr_pipeline.sh
+++ shell_scripts/run_pre_ovr_pipeline.sh
@@ -30,16 +30,18 @@ SCRIPTS=(
 )
 
 # Activate virtual environment
-source "${BASE_DIR}/.venv/Script/activate"
+source "${BASE_DIR}/.venv/Scripts/activate"
 
-# Execute Python scripts in sequence, passing the date parameter
+# Execute Python scripts in sequence as modules
 for script in "${SCRIPTS[@]}"; do
-    echo "Running $script"
-    python "${BASE_DIR}/scripts/${script}" "$DATE"
+    script_module=${script%.py}
+    script_module=${script_module//\//.} # Convert file path to module path
+    echo "Running $script_module"
+    python -m "scripts.${script_module}" "$DATE"
     
     # Check if the script executed successfully
     if [ $? -ne 0 ]; then
-        echo "Error occurred while running $script"
+        echo "Error occurred while running $script_module"
         exit 1
     fi
 done
