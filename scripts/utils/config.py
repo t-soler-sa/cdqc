cf17d1b0632100ab66452818f2d9100c6ab7a995
Tristan Soler <tristan.soler@santanderam.com>
2025-04-28 17:45:32 +0200

    updated config files with new paths and feature to launch the get date app or not

--- scripts/utils/config.py
+++ scripts/utils/config.py
@@ -10,7 +10,11 @@ from .get_output_dir import get_output_dir
 
 
 def get_config(
-    script_name: str = "default", interactive: bool = True, gen_output_dir: bool = True
+    script_name: str = "default",
+    interactive: bool = False,
+    gen_output_dir: bool = False,
+    auto_date: bool = True,
+    fixed_date: str = None,
 ) -> dict:
     """
     Set up and return common configuration settings.
@@ -29,8 +33,14 @@ def get_config(
     # Initialize logger for the current script
     logger = set_up_log(script_name)
 
-    # Get the date in YYYYMM format.
-    DATE = get_date()  # Expected to return a valid date string in "YYYYMM" format.
+    if auto_date:
+        # Use the get_date function to retrieve the date in YYYYMM format.
+        DATE = get_date()  # Expected to return a valid date string in "YYYYMM" format.
+    else:
+        if fixed_date is None:
+            raise ValueError("fixed_date must be provided when get_date is False.")
+        DATE = fixed_date
+
     YEAR = DATE[:4]
     date_obj = datetime.strptime(DATE, "%Y%m")
     prev_date_obj = date_obj - relativedelta(months=1)
@@ -41,7 +51,12 @@ def get_config(
     REPO_DIR = Path(r"C:\Users\n740789\Documents\clarity_data_quality_controls")
     # DATAFEED_DIR is taken from an enviroment variable (with a fallback if needed)
     DATAFEED_DIR = Path(r"C:\Users\n740789\Documents\Projects_local\DataSets\DATAFEED")
-    SRI_DATA_DIR = REPO_DIR / "excel_books" / "sri_data"
+    EXCEL_BOOKS_DIR = REPO_DIR / "excel_books"
+    SRI_DATA_DIR = EXCEL_BOOKS_DIR / "sri_data"
+    ALADDIN_DATA_DIR = EXCEL_BOOKS_DIR / "aladdin_data"
+    NASDAQ_DATA_DIR = EXCEL_BOOKS_DIR / "nasdaq_data"
+    SUSTAINALYTICS_DATA_DIR = EXCEL_BOOKS_DIR / "sustainalytics_data"
+    ESG_METRICS_MAP_DIR = SRI_DATA_DIR / "esg_metrics"
 
     # Define paths that are common for many scripts
     paths = {
@@ -62,14 +77,10 @@ def get_config(
         / f"{YEAR}"
         / f"{DATE}01_Equities_feed_IssuerLevel_sinOVR.csv",
         "PROCESSED_DFS_WOUTOVR_PATH": DATAFEED_DIR / "ficheros_tratados" / f"{YEAR}",
-        "CROSSREFERENCE_PATH": REPO_DIR
-        / "excel_books"
-        / "aladdin_data"
+        "CROSSREFERENCE_PATH": ALADDIN_DATA_DIR
         / "crossreference"
         / f"Aladdin_Clarity_Issuers_{DATE}01.csv",
-        "BMK_PORTF_STR_PATH": REPO_DIR
-        / "excel_books"
-        / "aladdin_data"
+        "BMK_PORTF_STR_PATH": ALADDIN_DATA_DIR
         / "bmk_portf_str"
         / f"{DATE}_strategies_snt world_portf_bmks.xlsx",
         "OVR_PATH": SRI_DATA_DIR / "overrides" / "overrides_db.xlsx",
@@ -78,6 +89,9 @@ def get_config(
         / "sri_data"
         / "portfolios_committees"
         / "portfolio_lists.xlsx",
+        "NASDAQ_DATA_PATH": NASDAQ_DATA_DIR / f"{DATE}_esg_nasdad_flag.xlsx",
+        "SUSTAINALYTICS_DATA_PATH": SUSTAINALYTICS_DATA_DIR
+        / f"{DATE}_sustainalytics_controversies.xlsx",
     }
 
     if gen_output_dir:
@@ -95,7 +109,12 @@ def get_config(
         "REPO_DIR": REPO_DIR,
         "DATAFEED_DIR": DATAFEED_DIR,
         "SRI_DATA_DIR": SRI_DATA_DIR,
+        "EXCEL_BOOKS_DIR": EXCEL_BOOKS_DIR,
+        "ALADDIN_DATA_DIR": ALADDIN_DATA_DIR,
+        "NASDAQ_DATA_DIR": NASDAQ_DATA_DIR,
+        "SUSTAINALYTICS_DATA_DIR": SUSTAINALYTICS_DATA_DIR,
         "paths": paths,
         "OUTPUT_DIR": OUTPUT_DIR,
+        "ESG_METRICS_MAP_DIR": ESG_METRICS_MAP_DIR,
     }
     return config
