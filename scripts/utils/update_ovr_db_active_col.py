9eb2df34b27ab42cf9ec17b21ab0fcbd35e27ae9
Tristan Soler <tristan.soler@santanderam.com>
2025-05-20 09:08:49 +0200

    updated the ovr databes active script - now it seems that it works as intended

--- scripts/utils/update_ovr_db_active_col.py
+++ scripts/utils/update_ovr_db_active_col.py
@@ -12,10 +12,19 @@ import sys
 import pandas as pd
 
 from scripts.utils.config import get_config
-from scripts.utils.dataloaders import load_clarity_data
+from scripts.utils.dataloaders import (
+    load_clarity_data,
+    load_overrides,
+    load_crossreference,
+)
 
-config = get_config("update_ovr_db_active_col", interactive=False, gen_output_dir=False)
+# config script
+config = get_config("update-ovr-db-active-col", interactive=False, gen_output_dir=False)
 logger = config["logger"]
+paths = config["paths"]
+df_path = paths["CURRENT_DF_WOUTOVR_PATH"]
+overrides_path = paths["OVR_PATH"]
+CROSSREFERENCE_PATH = paths["CROSSREFERENCE_PATH"]
 
 clarity_test_col = [
     "permid",
@@ -25,21 +34,25 @@ clarity_test_col = [
     "str_003b_ec",
     "str_004_asec",
     "str_005_ec",
-    "art_8_basicos",
     "str_006_sec",
+    "str_007_sect",
+    "art_8_basicos",
     "cs_001_sec",
     "cs_002_ec",
 ]
 
-DATE = datetime.now().strftime("%Y%m%d")
+DATE = config["DATE"]
 
-
-def load_overrides(path):
-    try:
-        return pd.read_excel(path)
-    except Exception as e:
-        logger.error(f"Failed to load overrides file: {e}")
-        raise
+target_cols_overrides = [
+    "permid",
+    "aladdin_id",
+    "clarityid",
+    "issuer_name",
+    "ovr_target",
+    "ovr_value",
+    "ovr_active",
+    "df_value",
+]
 
 
 def update_df_value_column(
@@ -47,23 +60,23 @@ def update_df_value_column(
 ) -> pd.DataFrame:
     """
     Updates the 'df_value' column in the overrides DataFrame using values
-    from the df_clarity_filtered DataFrame based on matching 'permid' and 'ovr_target'.
+    from the df_clarity_filtered DataFrame based on matching 'aladdin_id' and 'ovr_target'.
 
     Parameters:
-    - overrides: DataFrame containing override entries with 'permid' and 'ovr_target' columns.
-    - df_clarity_filtered: Filtered DataFrame from Clarity data with columns including 'permid'.
+    - overrides: DataFrame containing override entries with 'aladdin_id' and 'ovr_target' columns.
+    - df_clarity_filtered: Filtered DataFrame from Clarity data with columns including 'aladdin_id'.
 
     Returns:
     - A new DataFrame with the updated 'df_value' column.
     """
     # Melt df_clarity_filtered to long format for easier matching
     clarity_melted = df_clarity_filtered.melt(
-        id_vars="permid", var_name="ovr_target", value_name="clarity_value"
+        id_vars="aladdin_id", var_name="ovr_target", value_name="clarity_value"
     )
 
     # Merge to bring in the matching clarity value
     overrides_updated = overrides.merge(
-        clarity_melted, how="left", on=["permid", "ovr_target"]
+        clarity_melted, how="left", on=["aladdin_id", "ovr_target"]
     )
 
     # Update the df_value column only where clarity_value is present
@@ -118,22 +131,31 @@ def update_override_active(
 
 
 def main():
-    # config & load data
-    df_path = config["paths"]["CURRENT_DF_WOUTOVR_PATH"]
-    df_clarity = load_clarity_data(df_path, target_cols=clarity_test_col)
-    overrides_path = config["paths"]["OVR_PATH"]
-    overrides = load_overrides(overrides_path)
+    # load data
 
+    df_clarity = load_clarity_data(df_path, target_cols=clarity_test_col)
+    overrides = load_overrides(
+        overrides_path, target_cols=target_cols_overrides, drop_active=False
+    )
     # save back columns for backup
     overrides_copy = overrides.copy()
+    if "brs_id" in overrides.columns:
+        overrides.rename(columns={"brs_id": "aladdin_id"}, inplace=True)
+    crossreference = load_crossreference(CROSSREFERENCE_PATH)
+    logger.info(f"override columns are {overrides.columns}")
+    logger.info
+    # add aladdin_id to df_clarity from crossreference
+    df_clarity = df_clarity.merge(
+        crossreference[["permid", "aladdin_id"]], on="permid", how="left"
+    )
 
     # set permid in df_clarity & overrides as str
-    df_clarity["permid"] = df_clarity["permid"].astype(str)
-    overrides["permid"] = overrides["permid"].astype(str)
+    df_clarity["aladdin_id"] = df_clarity["aladdin_id"].astype(str)
+    overrides["aladdin_id"] = overrides["aladdin_id"].astype(str)
 
     # filter out only the columns we need with using the relevant permids
     df_clarity_filtered = df_clarity[
-        df_clarity["permid"].isin(overrides["permid"])
+        df_clarity["aladdin_id"].isin(overrides["aladdin_id"])
     ].copy()
 
     if df_clarity_filtered.shape[0] < 1:
@@ -146,9 +168,12 @@ def main():
     base_path = Path(
         r"C:\Users\n740789\Documents\clarity_data_quality_controls\excel_books\sri_data\overrides"
     )
+    current_date = datetime.now().strftime("%Y%m%d")
     output_file = base_path / "overrides_db_beta.xlsx"
     backup_file = base_path / "overrides_db_backup" / f"{DATE}_override_db.xlsx"
-    deactivated_overrides_file = base_path / f"{DATE}_deactivated_overrides.xlsx"
+    deactivated_overrides_file = (
+        base_path / f"{current_date}_{DATE}_deactivated_overrides.xlsx"
+    )
 
     # update active status of overrides
     logger.info("updating overrides active status")
@@ -158,19 +183,15 @@ def main():
     logger.info("Updating overrides df_value column")
     overrides = update_df_value_column(overrides, df_clarity_filtered)
 
-    overrides = overrides[overrides_copy.columns]
-
-    logger.info(
-        f"Saving updated overrides to {output_file}\nand backup to {backup_file}"
-    )
-
     # RETURN DF OF OVERRIDES THAT HAS BEEN DEACTIVATED
     deactivated_overrides = overrides[overrides["ovr_active"] == False]
     # log length of deactivated overrides
     logger.info(f"Number of deactivated overrides: {len(deactivated_overrides)}")
 
     # save the updated overrides to the output file
-    logger.info(f"Saving updated overrides to {output_file}")
+    logger.info(
+        f"Saving updated overrides to {output_file}\nand backup to {backup_file}"
+    )
     overrides.to_excel(output_file, index=False)
     overrides_copy.to_excel(backup_file, index=False)
     deactivated_overrides.to_excel(deactivated_overrides_file, index=False)
