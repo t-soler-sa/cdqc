bfd27537bd0744ceaabd86482d1cbdc78ed5baf4
Tristan Soler <tristan.soler@santanderam.com>
2025-05-23 12:30:55 +0200

    first commit of script to filter log message from update override script

---
+++ scripts/utils/filter_log_update_override_db.py
@@ -0,0 +1,52 @@
+import re
+from pathlib import Path
+from datetime import datetime
+
+# import logging
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def remove_duplicate_warnings(input_path, output_path):
+    logger.info(f"Checking path: {input_path}")
+    if not input_path.exists():
+        logger.warning("❌ File does not exist!")
+        return
+    seen = set()
+    pattern = re.compile(r"WARNING\s*-\s*(.*)")
+
+    with open(input_path, "r", encoding="utf-8") as infile, open(
+        output_path, "w", encoding="utf-8"
+    ) as outfile:
+        for i, line in enumerate(infile, 1):
+            match = pattern.search(line)
+            if match:
+                message = match.group(1).strip()
+                if message not in seen:
+                    # Post-process message
+                    marker = "update_override_active()] - "
+                    if marker in message:
+                        message = message.split(marker, 1)[1].strip()
+                    outfile.write(message + "\n")
+                    seen.add(message)
+            else:
+                pass
+
+
+def main():
+    # today date in yyyymmdd format
+    today = datetime.now().strftime("%Y%m%d")
+    path_dir = Path(
+        rf"C:\Users\n740789\Documents\clarity_data_quality_controls\log\{today}_logs\update-ovr-db-active-col"
+    )
+    path_in = path_dir / f"update-ovr-db-active-col_{today}.log"
+    logger.info(f"Filtering log: {path_in}")
+    path_out = path_dir / "update-ovr-db-active-col-filtered.log"
+    remove_duplicate_warnings(path_in, path_out)
+    logger.info(f"Filtered log saved to: {path_out}")
+
+
+if __name__ == "__main__":
+    main()
