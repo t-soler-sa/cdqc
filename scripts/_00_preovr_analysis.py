0f39b3fc0dff8ccf23a0ef467c723ff73cc5a476
Tristan Soler <tristan.soler@santanderam.com>
2025-05-29 17:02:36 +0200

    removed the heading "00" from the config named

--- scripts/_00_preovr_analysis.py
+++ scripts/_00_preovr_analysis.py
@@ -50,7 +50,7 @@ from scripts.utils.config import get_config
 # CONFIG SCRIPT
 # Get the common configuration for the Pre-OVR-Analysis script.
 config = get_config(
-    "00-pre-ovr-analysis", interactive=False, gen_output_dir=True, output_dir_dated=True
+    "pre-ovr-analysis", interactive=False, gen_output_dir=True, output_dir_dated=True
 )
 logger = config["logger"]
 DATE = config["DATE"]
